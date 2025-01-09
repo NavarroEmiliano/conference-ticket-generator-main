96f7b0413b15e7560adf6aa5bfe058d3589158ce
Emiliano Navarro <emilianonavarro1993@gmail.com>
2025-01-09 20:26:03 -0300

    font file

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import "./Header.module.css";
+import logoMark from "../assets/images/logo-mark.svg";
+
+const Header = () => {
+  return (
+    <header>
+      <img src={logoMark} alt="" />
+      <h4>Coding Conf</h4>
+    </header>
+  );
+};
+
+export default Header;
