07e019e3331761bf30042967d7352f01bf1d9ab1
Emiliano Navarro <emilianonavarro1993@gmail.com>
2025-01-10 17:55:54 -0300

    user state

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
+import { useState } from "react";
 import "./App.css";
 import Form from "./components/Form";
 import Header from "./components/Header";
 
+export interface User {
+  userImg?: File | null;
+  fullName?: string;
+  email?: string;
+  [key: string]: string | File | null | undefined;
+}
+
 function App() {
+  const [user, setUser] = useState<User>({});
+
+  console.log(user);
+
   return (
     <>
       <Header />
-      <Form />
+      <Form user={user} setUser={setUser} />
     </>
   );
 }
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,18 +1,55 @@
 import styles from "./Form.module.css";
 import uploadIcon from "../assets/images/icon-upload.svg";
 import infoIcon from "../assets/images/icon-info.svg";
+import { ChangeEvent, useState } from "react";
+import { User } from "../App";
 
-const Form = () => {
+const Form = ({
+  setUser,
+  user,
+}: {
+  setUser: React.Dispatch<React.SetStateAction<User>>;
+  user: User;
+}) => {
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value, files, type } = e.target;
+    setUser((prev: User) => {
+      if (type === "file" && files) {
+        return {
+          ...prev,
+          userImg: files[0],
+        };
+      }
+
+      return {
+        ...prev,
+        [name]: value,
+      };
+    });
+  };
+
+  const handleSubmit = (e: ChangeEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+
   return (
     <div className={styles.container}>
       <h2>Your Journey to Coding Conf 2025 Starts Here!</h2>
       <p>Secure your spot at next year's biggest coding conference.</p>
-      <form className={styles.form}>
+      <form onSubmit={handleSubmit} className={styles.form}>
         <label className={styles.fileLabel} htmlFor="file">
           Upload Avatar
         </label>
         <div className={styles.fileBox}>
-          <input type="file" name="file" id="file" />
+          <input
+            onChange={handleChange}
+            type="file"
+            name="file"
+            id="file"
+            accept="image/jpg, image/png"
+          />
           <div className={styles.iconContainer}>
             <img className={styles.icon} src={uploadIcon} alt="Upload Icon" />
           </div>
@@ -20,19 +57,36 @@ const Form = () => {
         </div>
         <div className={styles.infoBox}>
           <img src={infoIcon} alt="Icon info" />
-          <p>Upload your photo (JP or PNG, max size: 500kb)</p>.
+          <p>Upload your photo (JPG or PNG, max size: 500kb)</p>.
         </div>
         <div className={styles.inputBox}>
-          <label htmlFor="name">Full Name</label>
-          <input type="text" id="name"/>
+          <label htmlFor="username">Full Name</label>
+          <input
+            onChange={handleChange}
+            type="text"
+            id="username"
+            name="username"
+          />
         </div>
         <div className={styles.inputBox}>
           <label htmlFor="email">Email Address</label>
-          <input type="email" id="email" placeholder="example@gmail.com"/>
+          <input
+            onChange={handleChange}
+            type="email"
+            id="email"
+            name="email"
+            placeholder="example@gmail.com"
+          />
         </div>
         <div className={styles.inputBox}>
           <label htmlFor="github">GitHub Username</label>
-          <input type="text" id="github" placeholder="@yourusername"/>
+          <input
+            onChange={handleChange}
+            type="text"
+            id="github"
+            name="github"
+            placeholder="@yourusername"
+          />
         </div>
         <button type="submit">Generate My Ticket</button>
       </form>
